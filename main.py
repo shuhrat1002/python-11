36bd903c6df3435376c0236736fedf8077ef065e
shuhrat sila <126439094+shuhrat1002@users.noreply.github.com>
2023-03-04 10:54:13 +0500

    PYTHON

---
+++ main.py
@@ -0,0 +1,24 @@
+# Topshiriq-1-Foydalanuvchi ismini.... terminalga o'tish
+# fristname = input ("Ismizni kiriting")
+# lastname = ("familangiz nima")
+# age = ("yoshingiz nima")
+# email = ("enmail kiritng")
+# password = int(input("parolni kiriting"))
+# print (frintname,lastname,age,email,password)
+             #Topshiriq -2-masala yechish
+# 1) Ifodani yeching
+# a = (12*(5/6)+2*(7/9))*(15*(9/10)-12*(9/10))
+# print(a)                                                                                                                                                                                                                                             
+
+#  2) Ifodani yeching
+# a = (5*(7/16)*1*(3/29)+2*(5/16)*2*(2/7))
+# print(a)
+
+# 3)hisoblang
+# s = (15*13+15*29+42*19+34*58)
+# print(s)
+
+# 4) a=4
+# b=6
+# z=a**2+2*a*b+b**2
+# print(z)
