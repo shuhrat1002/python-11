2f687a671275341d2d976c4d57ffebd138ce211f
shuhrat sila <126439094+shuhrat1002@users.noreply.github.com>
2023-03-07 10:36:19 +0500

    Add files via upload

---
+++ Новая папка/main.py
@@ -0,0 +1,35 @@
+# Birinchi
+a = int(input("sonni kiriting:"))
+if (a % 3 == 0 and a % 5 == 0):
+    print("Fizz Buzz")
+elif (a % 3 == 0):
+    print("Fizz")
+elif (a % 5 == 0):
+   print("Buzz")
+else:
+   print("xatolik!")
+
+#ikkinchi
+login = input("loginni kiriting")
+email = input("emailni kiriting")
+parol = input("parolni kiriting")
+
+login == 2010
+email == "ibadillayevshuhrat@gmail.com"
+parol == 2000000200
+
+if(login == 2010 and email == "ibadillayevshuhrat@gmail.com" and parol == 2000000200):
+   print("Admin: xush kelibsiz")
+
+# uchinchi
+print ("Maxanizatsiyalashtirishmoqchiliginiaytganinibilibqoldim") [0:13]
+print ("Maxanizatsiyalashtirishmoqchiliginiaytganinibilibqoldim") [0:53]
+
+# to'rtinchi  
+a = int(input("1-sonni kiriting"))
+b = int(input("2-sonni kiriting"))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print(c, d, e, f, sep="=>")
