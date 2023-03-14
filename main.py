8183ff1689b7424dd69f86034b7a20b1564f413e
shuhrat sila <126439094+shuhrat1002@users.noreply.github.com>
2023-03-14 10:21:45 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,27 @@
 # print("1abc2das3dbc4ghj5"[::4])
 
 # uchinchi
-def replace_python_with_java(text):
-    return text.replace("Python", "JavaScript")
-input_text = "Python is most popular programming language"
-output_text = replace_python_with_java(input_text)
-print(output_text)
+# def replace_python_with_java(text):
+#     return text.replace("Python", "JavaScript")
+# input_text = "Python is most popular programming language"
+# output_text = replace_python_with_java(input_text)
+# print(output_text)
+
+# to'rtinchi
+# 1) def obhavo(gradus):
+#     if gradus <= 10:
+#        return "obhavo issiq"
+#     elif 10 < gradus <= 27:
+#        return  "obhavo salqin"
+#     else:
+#         return "obhavo iliq"
+# print(obhavo(5))
+# 2) def ovqatber():
+#     print("lavash ol")
+# ovqatber()
+# 3) def odamniur():
+#     print("elbekni ur")
+# odamniur()
+def musiqa():
+    print("musiqa tingla")
+musiqa()
